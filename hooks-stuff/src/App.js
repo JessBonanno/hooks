80a09c37d7f5c96eb5790b9fac11c0e9cd75e44c
jbonanno <jbonanno@slack-corp.com>
2021-11-14 17:47:18 -0500

    axios search call

--- hooks-stuff/src/App.js
+++ hooks-stuff/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const initialForm = {
   search: '',
 };
+
 
 const App = () => {
   const [form, setForm] = useState(initialForm);
@@ -18,21 +20,32 @@ const App = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(form)
+    searchJokes();
+  }
+
+  const searchJokes = async() => {
+    try {
+      const jokes = await axios.get(`https://api.chucknorris.io/jokes/search?query=${form.search}`);
+      console.log(jokes.data);
+    } catch (err) {
+      console.error(err);
+    }
+
   }
 
   return (
     <div className="App">
-      <fom>
+      <form>
         <label htmlFor='search'>Search</label>Î
         <input 
         id='search' 
         type="text" 
         name='search' 
+        value={form.search}
         placeholder="Search" 
         onChange={changeHandler} />
         <button type='submit' onClick={submitHandler}>Submit</button>
-      </fom>
+      </form>
     </div>
   );
 }
