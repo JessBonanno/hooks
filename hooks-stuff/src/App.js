d6cf62df1d960bd93336f19a36c6a4fefcec15f8
jbonanno <jbonanno@slack-corp.com>
2021-11-14 17:36:06 -0500

    created handlers

--- hooks-stuff/src/App.js
+++ hooks-stuff/src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
-import React, { UseState } from 'react';
+import React, { useState } from 'react';
 
-function App() {
+const initialForm = {
+  search: '',
+};
 
+const App = () => {
+  const [form, setForm] = useState(initialForm);
+
+
+  const changeHandler = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    })
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(form)
+  }
 
   return (
     <div className="App">
       <fom>
         <label htmlFor='search'>Search</label>Î
-        <input id='search' type="text" name='search' placeholder="Search" />
-        <input type='submit'/>
+        <input 
+        id='search' 
+        type="text" 
+        name='search' 
+        placeholder="Search" 
+        onChange={changeHandler} />
+        <button type='submit' onClick={submitHandler}>Submit</button>
       </fom>
     </div>
   );
